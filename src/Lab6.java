b3fe2b3fcd970bf00754810bc29bb07d5a5f2353
Ben Mendoza <bendoza@Bens-MacBook-Pro-3.local>
2022-03-14 02:45:10 -0400

    second commit

--- src/Lab6.java
+++ src/Lab6.java
@@ -4,5 +4,5 @@ public class Lab6 {
     Random rand = new Random();
     int num1 = rand.nextInt(100);
     int num2 = rand.nextInt(100);
-    int returnVal = num1 + num2;
+    int returnVal = num1 - num2;
 }
