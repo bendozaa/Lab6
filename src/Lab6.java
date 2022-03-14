c990f8a5cc4da0512737a23a225a1ccc5dacb354
Ben Mendoza <bendoza@Bens-MacBook-Pro-3.local>
2022-03-14 02:36:36 -0400

    first commit

---
+++ src/Lab6.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class Lab6 {
+    Random rand = new Random();
+    int num1 = rand.nextInt(100);
+    int num2 = rand.nextInt(100);
+    int returnVal = num1 + num2;
+}
